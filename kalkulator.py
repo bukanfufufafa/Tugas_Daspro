169be44bb72e2de1697a9580a8b91ef7d4f1d6cc
bukanfufufafa <nabilrizky249@gmail.com>
2024-11-09 09:41:27 +0700

    add calculator

---
+++ kalkulator.py
@@ -0,0 +1,9 @@
+#Nama: Nabil Rizky Ramadhan Setiawan
+#NIM: 2403272
+#Kelas: RPL 1B 
+
+a = int(input("masukkan angka a: "))
+b = int(input("masukkan angka b: "))
+c = int(input("masukkan angka c: "))
+d = a + b + c
+print(f"{a} + {b} + {c} = {d}")
