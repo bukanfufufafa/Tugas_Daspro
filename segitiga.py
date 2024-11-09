562621536aa85fa2023d99f16acb08f4e5cacbe3
bukanfufufafa <nabilrizky249@gmail.com>
2024-11-09 10:10:16 +0700

    add segitiga

---
+++ segitiga.py
@@ -0,0 +1,8 @@
+#Nama: Nabil Rizky Ramadhan Setiawan
+#NIM: 2403272
+#Kelas: RPL 1B 
+
+alas = float(input("masukkan panjang alas: "))
+tinggi = float(input("masukkan tinggi: "))
+luas = alas * tinggi * 0.5
+print(f"luas segitiga adalah {luas}")
